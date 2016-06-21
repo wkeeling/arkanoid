f8d31de8fe87b342eb411fb2a11da39109b5a4ce
Will Keeling <will@zifferent.com>
2016-06-21 22:35:14 +0100

    Unit tests

--- tests/test_sprites.py
+++ tests/test_sprites.py
@@ -33,6 +33,93 @@ class TestPaddle(TestCase):
         mock_pygame.Rect.assert_called_once_with(10, 630, 580, 10)
         self.assertEqual(paddle.rect.center, 'area center')
 
+    @patch('arkanoid.sprites.load_png')
+    @patch('arkanoid.sprites.pygame')
+    def test_update_moves_when_in_area(self, mock_pygame, mock_load_png):
+        mock_image, mock_rect, mock_area, mock_new_rect = (
+            Mock(), Mock(), Mock(), Mock())
+        mock_load_png.return_value = mock_image, mock_rect
+        mock_pygame.Rect.return_value = mock_area
+        mock_rect.move.return_value = mock_new_rect
+        mock_area.contains.return_value = True
+
+        paddle = Paddle()
+        paddle.update()
+
+        self.assertEqual(paddle.rect, mock_new_rect)
+
+    @patch('arkanoid.sprites.load_png')
+    @patch('arkanoid.sprites.pygame')
+    def test_update_not_move_when_not_in_area(self, mock_pygame, mock_load_png):
+        mock_image, mock_rect, mock_area, mock_new_rect = (
+            Mock(), Mock(), Mock(), Mock())
+        mock_load_png.return_value = mock_image, mock_rect
+        mock_pygame.Rect.return_value = mock_area
+        mock_rect.move.return_value = mock_new_rect
+        mock_area.contains.return_value = False
+
+        paddle = Paddle()
+        paddle.update()
+
+        self.assertEqual(paddle.rect, mock_rect)
+
+    @patch('arkanoid.sprites.load_png')
+    @patch('arkanoid.sprites.pygame')
+    def test_move_left(self, mock_pygame, mock_load_png):
+        mock_image, mock_rect, mock_area = (Mock(), Mock(), Mock())
+        mock_load_png.return_value = mock_image, mock_rect
+        mock_pygame.Rect.return_value = mock_area
+        mock_area.contains.return_value = True
+
+        paddle = Paddle()
+        paddle.move_left()
+        paddle.update()
+
+        mock_rect.move.assert_called_once_with(-10, 0)
+
+    @patch('arkanoid.sprites.load_png')
+    @patch('arkanoid.sprites.pygame')
+    def test_move_right(self, mock_pygame, mock_load_png):
+        mock_image, mock_rect, mock_area = (Mock(), Mock(), Mock())
+        mock_load_png.return_value = mock_image, mock_rect
+        mock_pygame.Rect.return_value = mock_area
+        mock_area.contains.return_value = True
+
+        paddle = Paddle(speed=15)
+        paddle.move_right()
+        paddle.update()
+
+        mock_rect.move.assert_called_once_with(15, 0)
+
+    @patch('arkanoid.sprites.load_png')
+    @patch('arkanoid.sprites.pygame')
+    def test_stop(self, mock_pygame, mock_load_png):
+        mock_image, mock_rect, mock_area = (Mock(), Mock(), Mock())
+        mock_load_png.return_value = mock_image, mock_rect
+        mock_pygame.Rect.return_value = mock_area
+        mock_area.contains.return_value = True
+
+        paddle = Paddle()
+        paddle.stop()
+        paddle.update()
+
+        mock_rect.move.assert_called_once_with(0, 0)
+
+    @patch('arkanoid.sprites.load_png')
+    @patch('arkanoid.sprites.pygame')
+    def test_reset(self, mock_pygame, mock_load_png):
+        mock_image, mock_rect, mock_area = (Mock(), Mock(), Mock())
+        mock_load_png.return_value = mock_image, mock_rect
+        mock_pygame.Rect.return_value = mock_area
+        mock_area.contains.return_value = True
+        mock_area.center = 'the centre'
+
+        paddle = Paddle()
+        paddle.reset()
+        paddle.update()
+
+        self.assertEqual(mock_rect.center, 'the centre')
+
     def test_bounce_strategy(self):
         angles = []
         paddle = pygame.Rect(100, 600, 60, 15)
