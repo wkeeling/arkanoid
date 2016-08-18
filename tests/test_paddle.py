e413c9546c236bb24fb3d928541bd230b707edaf
Will Keeling <will@zifferent.com>
2016-08-18 21:18:34 +0100

    Fix paddle tests

--- tests/test_paddle.py
+++ tests/test_paddle.py
@@ -10,9 +10,11 @@ from arkanoid.sprites.paddle import Paddle
 
 class TestPaddle(TestCase):
 
+    @patch('arkanoid.sprites.paddle.load_png_sequence')
     @patch('arkanoid.sprites.paddle.load_png')
     @patch('arkanoid.sprites.paddle.pygame')
-    def test_initialise(self, mock_pygame, mock_load_png):
+    def test_initialise(self, mock_pygame, mock_load_png,
+                        mock_load_png_sequence):
         mock_screen, mock_area, mock_image, mock_rect = (
             Mock(), Mock(), Mock(), Mock())
         mock_screen.left = 0
@@ -33,9 +35,11 @@ class TestPaddle(TestCase):
         mock_pygame.Rect.assert_called_once_with(10, 630, 580, 10)
         self.assertEqual(paddle.rect.center, 'area center')
 
+    @patch('arkanoid.sprites.paddle.load_png_sequence')
     @patch('arkanoid.sprites.paddle.load_png')
     @patch('arkanoid.sprites.paddle.pygame')
-    def test_update_moves_when_in_area(self, mock_pygame, mock_load_png):
+    def test_update_moves_when_in_area(self, mock_pygame, mock_load_png,
+                                       mock_load_png_sequence):
         mock_image, mock_rect, mock_area, mock_new_rect = (
             Mock(), Mock(), Mock(), Mock())
         mock_load_png.return_value = mock_image, mock_rect
@@ -49,10 +53,12 @@ class TestPaddle(TestCase):
 
         self.assertEqual(paddle.rect, mock_new_rect)
 
+    @patch('arkanoid.sprites.paddle.load_png_sequence')
     @patch('arkanoid.sprites.paddle.load_png')
     @patch('arkanoid.sprites.paddle.pygame')
     def test_update_not_move_when_not_in_area(self, mock_pygame,
-                                              mock_load_png):
+                                              mock_load_png,
+                                              mock_load_png_sequence):
         mock_image, mock_rect, mock_new_rect, mock_area_contains = (
             Mock(), Mock(), Mock(), Mock())
         mock_load_png.return_value = mock_image, mock_rect
@@ -66,9 +72,11 @@ class TestPaddle(TestCase):
 
         self.assertEqual(paddle.rect, mock_rect)
 
+    @patch('arkanoid.sprites.paddle.load_png_sequence')
     @patch('arkanoid.sprites.paddle.load_png')
     @patch('arkanoid.sprites.paddle.pygame')
-    def test_move_left(self, mock_pygame, mock_load_png):
+    def test_move_left(self, mock_pygame, mock_load_png,
+                       mock_load_png_sequence):
         mock_image, mock_rect, mock_area = (Mock(), Mock(), Mock())
         mock_load_png.return_value = mock_image, mock_rect
         mock_pygame.Rect.return_value = mock_area
@@ -80,9 +88,11 @@ class TestPaddle(TestCase):
 
         mock_rect.move.assert_called_once_with(-10, 0)
 
+    @patch('arkanoid.sprites.paddle.load_png_sequence')
     @patch('arkanoid.sprites.paddle.load_png')
     @patch('arkanoid.sprites.paddle.pygame')
-    def test_move_right(self, mock_pygame, mock_load_png):
+    def test_move_right(self, mock_pygame, mock_load_png,
+                        mock_load_png_sequence):
         mock_image, mock_rect, mock_area = (Mock(), Mock(), Mock())
         mock_load_png.return_value = mock_image, mock_rect
         mock_pygame.Rect.return_value = mock_area
@@ -94,9 +104,11 @@ class TestPaddle(TestCase):
 
         mock_rect.move.assert_called_once_with(15, 0)
 
+    @patch('arkanoid.sprites.paddle.load_png_sequence')
     @patch('arkanoid.sprites.paddle.load_png')
     @patch('arkanoid.sprites.paddle.pygame')
-    def test_stop(self, mock_pygame, mock_load_png):
+    def test_stop(self, mock_pygame, mock_load_png,
+                  mock_load_png_sequence):
         mock_image, mock_rect, mock_area = (Mock(), Mock(), Mock())
         mock_load_png.return_value = mock_image, mock_rect
         mock_pygame.Rect.return_value = mock_area
@@ -109,9 +121,11 @@ class TestPaddle(TestCase):
 
         self.assertEqual(mock_rect.move.call_count, 0)
 
+    @patch('arkanoid.sprites.paddle.load_png_sequence')
     @patch('arkanoid.sprites.paddle.load_png')
     @patch('arkanoid.sprites.paddle.pygame')
-    def test_reset(self, mock_pygame, mock_load_png):
+    def test_reset(self, mock_pygame, mock_load_png,
+                   mock_load_png_sequence):
         mock_image, mock_rect, mock_area = (Mock(), Mock(), Mock())
         mock_load_png.return_value = mock_image, mock_rect
         mock_pygame.Rect.return_value = mock_area
@@ -133,9 +147,9 @@ class TestPaddle(TestCase):
             angle = Paddle.bounce_strategy(paddle, ball)
             angles.append(int(math.degrees(angle)))
 
-        self.assertEqual(angles[0], -140)
-        self.assertEqual(angles[1], -115)
-        self.assertEqual(angles[2], -100)
-        self.assertEqual(angles[3], -80)
-        self.assertEqual(angles[4], -65)
-        self.assertEqual(angles[5], -40)
+        self.assertEqual(angles[0], 220)
+        self.assertEqual(angles[1], 245)
+        self.assertEqual(angles[2], 260)
+        self.assertEqual(angles[3], 280)
+        self.assertEqual(angles[4], 295)
+        self.assertEqual(angles[5], 320)
