f49e643e1dac635b647659e37ed9065a7547c76a
Will Keeling <will@zifferent.com>
2016-09-07 16:30:25 +0100

    Unit tests for CatchPowerUp

--- tests/test_powerup.py
+++ tests/test_powerup.py
@@ -2,7 +2,8 @@ from unittest import TestCase
 from unittest.mock import (Mock,
                            patch)
 
-from arkanoid.sprites.powerup import (ExpandPowerUp,
+from arkanoid.sprites.powerup import (CatchPowerUp,
+                                      ExpandPowerUp,
                                       ExtraLifePowerUp,
                                       LaserPowerUp,
                                       PowerUp,
@@ -200,25 +201,31 @@ class TestPowerUp(TestCase):
         self.assertFalse(powerup.visible)
 
 
+def _configure_mocks(mock_load_png_sequence, mock_pygame):
+    mock_image = Mock()
+    mock_load_png_sequence.return_value = [(mock_image, Mock())]
+    mock_rect = Mock()
+    mock_pygame.Rect.return_value = mock_rect
+    mock_rect.move.return_value = mock_rect
+    mock_rect.colliderect.return_value = True
+    mock_screen = Mock()
+    mock_pygame.display.get_surface.return_value = mock_screen
+    mock_area = Mock()
+    mock_screen.get_rect.return_value = mock_area
+    mock_area.contains.return_value = True
+    mock_game = Mock()
+    mock_game.paddle.exploding = False
+    mock_game.paddle.visible = True
+
+    return mock_game
+
+
 class TestExtraLifePowerUp(TestCase):
     @patch('arkanoid.sprites.powerup.load_png_sequence')
     @patch('arkanoid.sprites.powerup.pygame')
     def test_activate_adds_life_to_game(self, mock_pygame,
                                         mock_load_png_sequence):
-        mock_image = Mock()
-        mock_load_png_sequence.return_value = [(mock_image, Mock())]
-        mock_rect = Mock()
-        mock_pygame.Rect.return_value = mock_rect
-        mock_rect.move.return_value = mock_rect
-        mock_rect.colliderect.return_value = True
-        mock_screen = Mock()
-        mock_pygame.display.get_surface.return_value = mock_screen
-        mock_area = Mock()
-        mock_screen.get_rect.return_value = mock_area
-        mock_area.contains.return_value = True
-        mock_game = Mock()
-        mock_game.paddle.exploding = False
-        mock_game.paddle.visible = True
+        mock_game = _configure_mocks(mock_load_png_sequence, mock_pygame)
         mock_game.lives = 2
         powerup = ExtraLifePowerUp(mock_game, Mock())
 
@@ -231,20 +238,7 @@ class TestExtraLifePowerUp(TestCase):
     @patch('arkanoid.sprites.powerup.pygame')
     def test_deactivate_is_noop(self, mock_pygame,
                                 mock_load_png_sequence):
-        mock_image = Mock()
-        mock_load_png_sequence.return_value = [(mock_image, Mock())]
-        mock_rect = Mock()
-        mock_pygame.Rect.return_value = mock_rect
-        mock_rect.move.return_value = mock_rect
-        mock_rect.colliderect.return_value = True
-        mock_screen = Mock()
-        mock_pygame.display.get_surface.return_value = mock_screen
-        mock_area = Mock()
-        mock_screen.get_rect.return_value = mock_area
-        mock_area.contains.return_value = True
-        mock_game = Mock()
-        mock_game.paddle.exploding = False
-        mock_game.paddle.visible = True
+        mock_game = _configure_mocks(mock_load_png_sequence, mock_pygame)
         mock_game.lives = 2
         mock_game.active_powerup = ExtraLifePowerUp(mock_game, Mock())
         powerup = PowerUp(mock_game, Mock(), 'test_prefix')
@@ -260,20 +254,7 @@ class TestSlowBallPowerUp(TestCase):
     @patch('arkanoid.sprites.powerup.pygame')
     def test_activate_slows_all_balls(self, mock_pygame,
                                       mock_load_png_sequence):
-        mock_image = Mock()
-        mock_load_png_sequence.return_value = [(mock_image, Mock())]
-        mock_rect = Mock()
-        mock_pygame.Rect.return_value = mock_rect
-        mock_rect.move.return_value = mock_rect
-        mock_rect.colliderect.return_value = True
-        mock_screen = Mock()
-        mock_pygame.display.get_surface.return_value = mock_screen
-        mock_area = Mock()
-        mock_screen.get_rect.return_value = mock_area
-        mock_area.contains.return_value = True
-        mock_game = Mock()
-        mock_game.paddle.exploding = False
-        mock_game.paddle.visible = True
+        mock_game = _configure_mocks(mock_load_png_sequence, mock_pygame)
         mock_game.ball.base_speed = 10
         mock_game.balls = [Mock(), Mock(), Mock()]
         powerup = SlowBallPowerUp(mock_game, Mock())
@@ -290,20 +271,7 @@ class TestSlowBallPowerUp(TestCase):
     @patch('arkanoid.sprites.powerup.pygame')
     def test_deactivate_reinstates_ball_speed(self, mock_pygame,
                                               mock_load_png_sequence):
-        mock_image = Mock()
-        mock_load_png_sequence.return_value = [(mock_image, Mock())]
-        mock_rect = Mock()
-        mock_pygame.Rect.return_value = mock_rect
-        mock_rect.move.return_value = mock_rect
-        mock_rect.colliderect.return_value = True
-        mock_screen = Mock()
-        mock_pygame.display.get_surface.return_value = mock_screen
-        mock_area = Mock()
-        mock_screen.get_rect.return_value = mock_area
-        mock_area.contains.return_value = True
-        mock_game = Mock()
-        mock_game.paddle.exploding = False
-        mock_game.paddle.visible = True
+        mock_game = _configure_mocks(mock_load_png_sequence, mock_pygame)
         mock_game.active_powerup = SlowBallPowerUp(mock_game, Mock())
         mock_game.active_powerup._orig_speed = 10
         mock_game.balls = [Mock(), Mock(), Mock()]
@@ -329,20 +297,7 @@ class TestExpandPowerUp(TestCase):
     def test_activate_transitions_to_wide_state(self, mock_pygame,
                                                 mock_load_png_sequence,
                                                 mock_wide_state):
-        mock_image = Mock()
-        mock_load_png_sequence.return_value = [(mock_image, Mock())]
-        mock_rect = Mock()
-        mock_pygame.Rect.return_value = mock_rect
-        mock_rect.move.return_value = mock_rect
-        mock_rect.colliderect.return_value = True
-        mock_screen = Mock()
-        mock_pygame.display.get_surface.return_value = mock_screen
-        mock_area = Mock()
-        mock_screen.get_rect.return_value = mock_area
-        mock_area.contains.return_value = True
-        mock_game = Mock()
-        mock_game.paddle.exploding = False
-        mock_game.paddle.visible = True
+        mock_game = _configure_mocks(mock_load_png_sequence, mock_pygame)
         mock_state = Mock()
         mock_wide_state.return_value = mock_state
         mock_game.balls = [Mock(), Mock(), Mock()]
@@ -362,20 +317,7 @@ class TestExpandPowerUp(TestCase):
     @patch('arkanoid.sprites.powerup.pygame')
     def test_no_activate_when_already_expand(self, mock_pygame,
                                              mock_load_png_sequence):
-        mock_image = Mock()
-        mock_load_png_sequence.return_value = [(mock_image, Mock())]
-        mock_rect = Mock()
-        mock_pygame.Rect.return_value = mock_rect
-        mock_rect.move.return_value = mock_rect
-        mock_rect.colliderect.return_value = True
-        mock_screen = Mock()
-        mock_pygame.display.get_surface.return_value = mock_screen
-        mock_area = Mock()
-        mock_screen.get_rect.return_value = mock_area
-        mock_area.contains.return_value = True
-        mock_game = Mock()
-        mock_game.paddle.exploding = False
-        mock_game.paddle.visible = True
+        mock_game = _configure_mocks(mock_load_png_sequence, mock_pygame)
         mock_game.active_powerup = ExpandPowerUp(mock_game, Mock())
         powerup = ExpandPowerUp(mock_game, Mock())
         powerup._activate = Mock()
@@ -390,20 +332,7 @@ class TestExpandPowerUp(TestCase):
     def test_deactivate_transitions_to_normal_state(self, mock_pygame,
                                                     mock_load_png_sequence,
                                                     mock_normal_state):
-        mock_image = Mock()
-        mock_load_png_sequence.return_value = [(mock_image, Mock())]
-        mock_rect = Mock()
-        mock_pygame.Rect.return_value = mock_rect
-        mock_rect.move.return_value = mock_rect
-        mock_rect.colliderect.return_value = True
-        mock_screen = Mock()
-        mock_pygame.display.get_surface.return_value = mock_screen
-        mock_area = Mock()
-        mock_screen.get_rect.return_value = mock_area
-        mock_area.contains.return_value = True
-        mock_game = Mock()
-        mock_game.paddle.exploding = False
-        mock_game.paddle.visible = True
+        mock_game = _configure_mocks(mock_load_png_sequence, mock_pygame)
         mock_game.active_powerup = ExpandPowerUp(mock_game, Mock())
         mock_state = Mock()
         mock_normal_state.return_value = mock_state
@@ -429,20 +358,7 @@ class TestLaserPowerUp(TestCase):
     def test_activate_transitions_to_laser_state(self, mock_pygame,
                                                  mock_load_png_sequence,
                                                  mock_laser_state):
-        mock_image = Mock()
-        mock_load_png_sequence.return_value = [(mock_image, Mock())]
-        mock_rect = Mock()
-        mock_pygame.Rect.return_value = mock_rect
-        mock_rect.move.return_value = mock_rect
-        mock_rect.colliderect.return_value = True
-        mock_screen = Mock()
-        mock_pygame.display.get_surface.return_value = mock_screen
-        mock_area = Mock()
-        mock_screen.get_rect.return_value = mock_area
-        mock_area.contains.return_value = True
-        mock_game = Mock()
-        mock_game.paddle.exploding = False
-        mock_game.paddle.visible = True
+        mock_game = _configure_mocks(mock_load_png_sequence, mock_pygame)
         mock_state = Mock()
         mock_laser_state.return_value = mock_state
         mock_game.balls = [Mock(), Mock(), Mock()]
@@ -462,20 +378,7 @@ class TestLaserPowerUp(TestCase):
     @patch('arkanoid.sprites.powerup.pygame')
     def test_no_activate_when_already_laser(self, mock_pygame,
                                             mock_load_png_sequence):
-        mock_image = Mock()
-        mock_load_png_sequence.return_value = [(mock_image, Mock())]
-        mock_rect = Mock()
-        mock_pygame.Rect.return_value = mock_rect
-        mock_rect.move.return_value = mock_rect
-        mock_rect.colliderect.return_value = True
-        mock_screen = Mock()
-        mock_pygame.display.get_surface.return_value = mock_screen
-        mock_area = Mock()
-        mock_screen.get_rect.return_value = mock_area
-        mock_area.contains.return_value = True
-        mock_game = Mock()
-        mock_game.paddle.exploding = False
-        mock_game.paddle.visible = True
+        mock_game = _configure_mocks(mock_load_png_sequence, mock_pygame)
         mock_game.active_powerup = LaserPowerUp(mock_game, Mock())
         powerup = LaserPowerUp(mock_game, Mock())
         powerup._activate = Mock()
@@ -490,20 +393,7 @@ class TestLaserPowerUp(TestCase):
     def test_deactivate_transitions_to_normal_state(self, mock_pygame,
                                                     mock_load_png_sequence,
                                                     mock_normal_state):
-        mock_image = Mock()
-        mock_load_png_sequence.return_value = [(mock_image, Mock())]
-        mock_rect = Mock()
-        mock_pygame.Rect.return_value = mock_rect
-        mock_rect.move.return_value = mock_rect
-        mock_rect.colliderect.return_value = True
-        mock_screen = Mock()
-        mock_pygame.display.get_surface.return_value = mock_screen
-        mock_area = Mock()
-        mock_screen.get_rect.return_value = mock_area
-        mock_area.contains.return_value = True
-        mock_game = Mock()
-        mock_game.paddle.exploding = False
-        mock_game.paddle.visible = True
+        mock_game = _configure_mocks(mock_load_png_sequence, mock_pygame)
         mock_game.active_powerup = LaserPowerUp(mock_game, Mock())
         mock_state = Mock()
         mock_normal_state.return_value = mock_state
@@ -519,3 +409,92 @@ class TestLaserPowerUp(TestCase):
         self.assertEqual(len(mock_game.balls), 3)
         for ball in mock_game.balls:
             self.assertEqual(ball.base_speed, 10)
+
+
+class TestCatchPowerUp(TestCase):
+
+    @patch('arkanoid.sprites.powerup.receiver')
+    @patch('arkanoid.sprites.powerup.load_png_sequence')
+    @patch('arkanoid.sprites.powerup.pygame')
+    def test_activate_configures_catch(self, mock_pygame,
+                                       mock_load_png_sequence,
+                                       mock_receiver):
+        mock_game = _configure_mocks(mock_load_png_sequence, mock_pygame)
+        powerup = CatchPowerUp(mock_game, Mock())
+
+        powerup.update()
+
+        mock_load_png_sequence.assert_called_once_with('powerup_catch')
+        mock_game.paddle.ball_collide_callbacks.append.assert_called_once_with(
+            powerup._catch
+        )
+        mock_receiver.register_handler.assert_called_once_with(
+            mock_pygame.KEYUP, powerup._release_ball)
+
+    @patch('arkanoid.sprites.powerup.receiver')
+    @patch('arkanoid.sprites.powerup.load_png_sequence')
+    @patch('arkanoid.sprites.powerup.pygame')
+    def test_deactivate(self, mock_pygame,
+                        mock_load_png_sequence,
+                        mock_receiver):
+        mock_game = _configure_mocks(mock_load_png_sequence, mock_pygame)
+        catch_powerup = CatchPowerUp(mock_game, Mock())
+        mock_game.active_powerup = catch_powerup
+        mock_game.balls = [Mock(), Mock(), Mock()]
+        powerup = PowerUp(mock_game, Mock(), 'test_prefix')
+        powerup._activate = Mock()
+
+        powerup.update()
+
+        mock_game.paddle.ball_collide_callbacks.remove.assert_called_once_with(
+            catch_powerup._catch
+        )
+        mock_receiver.unregister_handler.assert_called_once_with(
+            catch_powerup._release_ball
+        )
+        for ball in mock_game.balls:
+            ball.release.assert_called_once_with()
+
+    @patch('arkanoid.sprites.powerup.load_png_sequence')
+    @patch('arkanoid.sprites.powerup.pygame')
+    def test_release_ball(self, mock_pygame, mock_load_png_sequence):
+        mock_game = _configure_mocks(mock_load_png_sequence, mock_pygame)
+        mock_game.balls = [Mock(), Mock(), Mock()]
+        mock_event = Mock()
+        mock_event.key = mock_pygame.K_SPACE  # Spacebar event
+
+        catch_powerup = CatchPowerUp(mock_game, Mock())
+        catch_powerup._release_ball(mock_event)
+
+        for ball in mock_game.balls:
+            ball.release.assert_called_once_with()
+
+    @patch('arkanoid.sprites.powerup.load_png_sequence')
+    @patch('arkanoid.sprites.powerup.pygame')
+    def test_release_ball_when_other_key_pressed(self, mock_pygame,
+                                                 mock_load_png_sequence):
+        mock_game = _configure_mocks(mock_load_png_sequence, mock_pygame)
+        mock_game.balls = [Mock(), Mock(), Mock()]
+        mock_event = Mock()
+        mock_event.key = mock_pygame.SOME_OTHER_KEY
+
+        catch_powerup = CatchPowerUp(mock_game, Mock())
+        catch_powerup._release_ball(mock_event)
+
+        for ball in mock_game.balls:
+            self.assertEqual(ball.call_count, 0)
+
+    @patch('arkanoid.sprites.powerup.load_png_sequence')
+    @patch('arkanoid.sprites.powerup.pygame')
+    def test_catch_anchors_ball(self, mock_pygame,
+                                mock_load_png_sequence):
+        mock_game = _configure_mocks(mock_load_png_sequence, mock_pygame)
+        mock_ball = Mock()
+        mock_ball.rect.bottomleft = [10]
+        mock_game.paddle.rect.topleft = [5]
+        mock_ball.rect.height = 5
+
+        catch_powerup = CatchPowerUp(mock_game, Mock())
+        catch_powerup._catch(mock_ball)
+
+        mock_ball.anchor.assert_called_once_with(mock_game.paddle, (5, -5))
